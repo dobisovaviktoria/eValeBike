166c16deed4960ad184ff92bd9f8dcec9aca6fbd
Deren Ozen <deren.ozen@student.kdg.be>
2025-02-17 16:01:38 +0100

    adding spring profiles

---
+++ src/main/java/integration4/evalebike/EValeBikeApplication.java
@@ -0,0 +1,13 @@
+package integration4.evalebike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EValeBikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EValeBikeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/integration4/evalebike/EValeBikeApplicationTests.java
@@ -0,0 +1,13 @@
+package integration4.evalebike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EValeBikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
