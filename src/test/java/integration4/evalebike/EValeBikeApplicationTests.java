85b0d65fe131c83f7328ed1fc0a0782a214fa2f3
abirbelhadj <abir.belhadj@student.kdg.be>
2025-04-30 21:59:06 +0200

    Changed Test environment to H2 database

--- src/test/java/integration4/evalebike/EValeBikeApplicationTests.java
+++ src/test/java/integration4/evalebike/EValeBikeApplicationTests.java
@@ -1,10 +1,33 @@
 package integration4.evalebike;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@Testcontainers
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(properties = "spring.profiles.active=test")
 class EValeBikeApplicationTests {
+
+    @Container
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15")
+            .withDatabaseName("testdb")
+            .withUsername("testuser")
+            .withPassword("testpass");
+
+    @DynamicPropertySource
+    static void dynamicProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 
     @Test
     void contextLoads() {
