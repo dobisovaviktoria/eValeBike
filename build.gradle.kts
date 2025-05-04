61a1791467249c6d56fe684b18decbe3f42b0751
abirbelhadj <abir.belhadj@student.kdg.be>
2025-05-04 11:44:25 +0200

    build job testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    java
-    id("org.springframework.boot") version "3.2.5"
-    id("io.spring.dependency-management") version "1.1.4"
+    id("java")
+    id("org.springframework.boot") version "3.4.2"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
 group = "integration4"
@@ -9,48 +9,37 @@ version = "0.0.1-SNAPSHOT"
 
 java {
     toolchain {
-        languageVersion.set(JavaLanguageVersion.of(21))
+        languageVersion = JavaLanguageVersion.of(21)
     }
 }
 
 repositories {
     mavenCentral()
-    maven { url = uri("https://jitpack.io") }
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
-    implementation("org.thymeleaf.extras:thymeleaf-extras-springsecurity6:3.1.2.RELEASE")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
-
-    runtimeOnly("org.postgresql:postgresql:42.7.5")
-    testImplementation("com.h2database:h2:2.2.232")
-
-    implementation("org.mapstruct:mapstruct:1.6.3")
-    annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
-
-    implementation("com.google.zxing:core:3.5.0")
-    implementation("com.google.zxing:javase:3.5.0")
+    implementation ("org.springframework.boot:spring-boot-starter-security")
+    runtimeOnly ("org.postgresql:postgresql:42.7.5")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     implementation("org.webjars:bootstrap:5.3.3")
     implementation("org.webjars.npm:bootstrap-icons:1.11.1")
+    implementation ("org.mapstruct:mapstruct:1.6.3")
+    annotationProcessor ("org.mapstruct:mapstruct-processor:1.6.3")
+    implementation ("org.springframework.boot:spring-boot-starter-webflux")
+
+    implementation ("com.fasterxml.jackson.dataformat:jackson-dataformat-xml")
+    implementation("com.google.zxing:core:3.5.0")
+    implementation("com.google.zxing:javase:3.5.0")
     implementation("org.webjars.npm:html5-qrcode:2.3.8")
+    implementation ("org.springframework.boot:spring-boot-starter-security")
+    implementation ("org.thymeleaf.extras:thymeleaf-extras-springsecurity6")
 
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.testcontainers:junit-jupiter:1.20.4")
-    testImplementation("org.testcontainers:postgresql:1.20.4")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
-    testImplementation("net.bytebuddy:byte-buddy:1.15.11")
-    testImplementation("net.bytebuddy:byte-buddy-agent:1.15.11")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.9.2")
-}
-
-tasks.named("build") {
-    dependsOn("bootJar")
 }
 
 tasks.withType<Test> {
