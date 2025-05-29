1406474f09e9713bb6b6b5e0ce371ec30f8a0e21
Lilit Tsugunyan <lilit.tsugunyan@student.kdg.be>
2025-05-29 18:34:32 +0200

    bike owner page

--- src/main/java/integration4/evalebike/controller/bikeOwner/BikeOwnerApiController.java
+++ src/main/java/integration4/evalebike/controller/bikeOwner/BikeOwnerApiController.java
@@ -1,9 +1,6 @@
 package integration4.evalebike.controller.bikeOwner;
 
-import integration4.evalebike.controller.bikeOwner.dto.BikeOwnerProfileDto;
-import integration4.evalebike.security.CustomUserDetails;
 import integration4.evalebike.service.BikeOwnerService;
-import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,17 +11,5 @@ public class BikeOwnerApiController {
     public BikeOwnerApiController(BikeOwnerService bikeOwnerService) {
         this.bikeOwnerService = bikeOwnerService;
     }
-
-//    @GetMapping("/profile")
-//    public BikeOwnerProfileDto getProfile(Authentication auth) {
-//        CustomUserDetails userDetails = (CustomUserDetails) auth.getPrincipal();
-//        return bikeOwnerService.getProfile(userDetails.getUserId());
-//    }
-//
-//    @PostMapping("/profile")
-//    public void updateProfile(@RequestBody BikeOwnerProfileDto dto, Authentication auth) {
-//        CustomUserDetails userDetails = (CustomUserDetails) auth.getPrincipal();
-//        bikeOwnerService.updateProfile(userDetails.getUserId(), dto);
-//    }
 }
 
